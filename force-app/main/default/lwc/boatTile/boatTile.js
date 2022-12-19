10d90109b34a2c5c451e004541ab761a41d2a7cf
Mustafa K. BAŞER <mkursatbaser@gmail.com>
2022-12-19 17:31:11 +0300

    6. Build the component boatTile

--- force-app/main/default/lwc/boatTile/boatTile.js
+++ force-app/main/default/lwc/boatTile/boatTile.js
@@ -9,23 +9,23 @@ export default class BoatTile extends LightningElement {
     
     // Getter for dynamically setting the background image for the picture
     get backgroundStyle() {
-      return "background-image:url(${this.boat.Picture__C})"
+      return "background-image:url(${this.boat.Picture__c})";
     }
     
     // Getter for dynamically setting the tile class based on whether the
     // current boat is selected
     get tileClass() {
-      return this.selectedBoatId ? TILE_WRAPPER_SELECTED_CLASS:TILE_WRAPPER_UNSELECTED_CLASS;
+      return this.selectedBoatId ? TILE_WRAPPER_SELECTED_CLASS : TILE_WRAPPER_UNSELECTED_CLASS;
     }
     
     // Fires event with the Id of the boat that has been selected.
     selectBoat() {
-      this.selectedBoatId =! this.selectedBoatId;
-      const selectBoat = new CustomEvent("selectBoat", {
+      this.selectedBoatId = !this.selectedBoatId;
+      const boatSelect = new CustomEvent("boatselect", {
         detail: {
           boatId: this.boat.Id
         }
       });
-      this.dispatchEvent(selectBoat);
+      this.dispatchEvent(boatSelect);
      }
   }
