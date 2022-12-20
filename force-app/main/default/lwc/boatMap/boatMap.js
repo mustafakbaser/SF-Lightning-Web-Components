56f4326aa2d3f0df93bb9d136442a2b8297f7c83
Mustafa K. BAŞER <mkursatbaser@gmail.com>
2022-12-20 11:22:32 +0300

    boatMap script created

--- force-app/main/default/lwc/boatMap/boatMap.js
+++ force-app/main/default/lwc/boatMap/boatMap.js
@@ -3,6 +3,28 @@ import { getRecord } from 'lightning/uiRecordApi';
 import BoatMessageChannel from '@salesforce/messageChannel/BoatMessageChannel__c';
 import { subscribe, MessageContext, APPLICATION_SCOPE } from 'lightning/messageService';
 
+// Declaring LONGITUDE and LATITUDE fields as const
+const LONGITUDE = "Boat__c.Geolocation__Longitude__s";
+const LATITUDE = "Boat__c.Geolocation__Latitude__s";
+
+// Declaring field combination for BOAT
+const BOAT_FIELDS = [LONGITUDE, LATITUDE];
+
 export default class BoatMap extends LightningElement {
-    
+    error = undefined;
+    locationMarkers=[];
+    subscription = null;
+    boatId;
+
+    @api
+    get recordId(){
+        return this.boatId;
+    }
+    set recordId(value){
+        this.setAttribute('boatId', value);
+        this.boatId = value;
+    }
+
+    @wire(MessageContext)
+    messageContext;
 }
