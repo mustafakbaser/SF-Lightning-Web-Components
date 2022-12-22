52baeaa45c73cbbc1a54df2eb15301abfe1b0844
Mustafa K. BAŞER <mkursatbaser@gmail.com>
2022-12-22 11:12:27 +0300

    boatsNearMe updated

--- force-app/main/default/lwc/boatsNearMe/boatsNearMe.js
+++ force-app/main/default/lwc/boatsNearMe/boatsNearMe.js
@@ -5,70 +5,76 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 const SUCCESS_TITLE = 'Success';
 const MESSAGE_SHIP_IT = 'Ship it!';
 const SUCCESS_VARIANT = 'success';
-const ERROR_TITLE   = 'Error';
+const ERROR_TITLE = 'Error loading Boats Near Me';
 const ERROR_VARIANT = 'error';
-const YOUR_LOCATION = 'Your current location';
-const STD_USER_ICON = 'standart:user';
+const LABEL_YOU_ARE_HERE = 'You are here!';
+const ICON_STANDARD_USER = 'standard:user';
 
-export default class BoatSearchResults extends LightningElement {
 
-    @api
-    boatTypeId;
-    mapMarkers = [];
-    isLoading = true;
-    isRendered;
-    latitude;
-    longitude;
+export default class BoatsNearMe extends LightningElement {
+    
+  @api
+  boatTypeId;
+  mapMarkers = [];
+  isLoading = true;
+  isRendered;
+  latitude;
+  longitude;
 
-    @wire(getBoatsByLocation, {latitude: '$latitude', longitude: '$longitude', boatTypeId: '$boatTypeId'})
-    wiredBoatsJSON({error, data}){
-      if(data){
-        this.createMapMarkers(data);
-      } else if(error){
-        const toast = new ShowToastEvent({
-          title: ERROR_TITLE,
-          message: error.message,
-          variant: ERROR_VARIANT
-        });
-        this.isLoading=false;
+ 
+  @wire(getBoatsByLocation, {latitude: '$latitude', longitude: '$longitude', boatTypeId: '$boatTypeId'})
+  wiredBoatsJSON({error, data}) {
+      if (data) {
+          this.createMapMarkers(data);
+      } else if (error) {
+          const toast = new ShowToastEvent({
+              title: ERROR_TITLE,
+              message: error.message,
+              variant: ERROR_VARIANT,
+          });
+          this.dispatchEvent(toast);
       }
-    }
+      this.isLoading = false;
+  }
 
-    renderedCallBack(){
-      if(!this.isRendered){
-        this.getLocationFromBrowser();
+ 
+  renderedCallback() {
+      if (!this.isRendered) {
+          this.getLocationFromBrowser();
       }
       this.isRendered = true;
-    }
+  }
 
-
-    getLocationFromBrowser(){
-      if(navigator.getLocation){
-        navigator.geolocation.getCurrentPosition(position => { 
-          this.latitude = position.coords.latitude;
-          this.longitude = position.coords.longitude;
-        });
+  // Gets the location from the Browser
+  // position => {latitude and longitude}
+  getLocationFromBrowser() {
+      if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(position => {
+              this.latitude = position.coords.latitude;
+              this.longitude = position.coords.longitude;
+          });
       }
-    }
+  }
 
-    createMapMarkers(boatData){
+  // Creates the map markers
+  createMapMarkers(boatData) {
       const newMarkers = JSON.parse(boatData).map(boat => {
-        return {
-          title: boat.Name,
-          location: {
-            latitude: boat.Geolocation__Latitude__s,
-            longitude: boat.Geolocation__Longitude__s
-          }
-        };
+          return {
+              title: boat.Name,
+              location: {
+                  Latitude: boat.Geolocation__Latitude__s,
+                  Longitude: boat.Geolocation__Longitude__s
+              }
+          };
       });
       newMarkers.unshift({
-        title:YOUR_LOCATION,
-        icon: STD_USER_ICON,
-        location:{
-          Latitude: this.latitude,
-          Longitude: this.longitude
-        }
+          title: LABEL_YOU_ARE_HERE,
+          icon: ICON_STANDARD_USER,
+          location: {
+              Latitude: this.latitude,
+              Longitude: this.longitude
+          }
       });
       this.mapMarkers = newMarkers;
-    }
+  }
 }
