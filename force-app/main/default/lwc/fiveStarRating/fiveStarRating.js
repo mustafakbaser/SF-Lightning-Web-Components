9f7dd53d2e95b5cef0583126e082cbc77de8ac12
Mustafa K. BAŞER <mkursatbaser@gmail.com>
2022-12-22 12:04:14 +0300

    Integrate Third Party Scripts to build the component fiveStarRating

--- force-app/main/default/lwc/fiveStarRating/fiveStarRating.js
+++ force-app/main/default/lwc/fiveStarRating/fiveStarRating.js
@@ -1,5 +1,7 @@
 import { LightningElement, api } from 'lwc';
-
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { loadStyle, loadScript } from 'lightning/platformResourceLoader';
+import fivestar from '@salesforce/resourceUrl/fivestar';
 
 // Defining constants
 const ERROR_TITLE = 'Error loading five-star';
@@ -14,4 +16,59 @@ export default class FiveStarRating extends LightningElement {
 
   editedValue;
   isRendered;
+
+  renderedCallback() {
+    if (this.isRendered) {
+      return;
+    }
+    this.loadScript();
+    this.isRendered = true;
+  }
+
+  get starClass() {
+    return this.readOnly ? READ_ONLY_CLASS : EDITABLE_CLASS;
+  }
+
+  initializeRating() {
+    let domEl = this.template.querySelector('ul');
+    let maxRating = 5;
+    let self = this;
+    let callback = function (rating) {
+      self.editedValue = rating;
+      self.ratingChanged(rating);
+    };
+    this.ratingObj = window.rating(
+      domEl,
+      this.value,
+      maxRating,
+      callback,
+      this.readOnly
+    );
+  }
+
+  loadScript() {
+    Promise.all([
+      loadScript(this, fivestar + '/rating.js'),
+      loadStyle(this, fivestar + '/rating.css')      
+    ]).then(() => {
+      this.initializeRating();
+    })
+    .catch(error => {
+      const toast = new ShowToastEvent({
+          title: ERROR_TITLE,
+          message: error.message,
+          variant: ERROR_VARIANT,
+      });
+      this.dispatchEvent(toast);
+    });
+  }
+
+  ratingChanged(rating) {     
+    const ratingchangeEvent = new CustomEvent('ratingchange', {
+      detail: {
+        rating: rating
+      }
+    });
+    this.dispatchEvent(ratingchangeEvent);    
+  }
 }
